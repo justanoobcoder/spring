7718db442f1b622ebfa08fc5691148e4e9835eb2
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-15 09:16:41 +0700

    added styles for choosen dependencies

--- ui/style/style.go
+++ ui/style/style.go
@@ -12,4 +12,11 @@ var (
 	PaginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
 	HelpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
 	QuitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	ChoosenTitleStyle = lipgloss.NewStyle().
+				Border(lipgloss.NormalBorder(), false, false, false, true).
+				BorderForeground(lipgloss.AdaptiveColor{Light: "#84f5a2", Dark: "#3cc962"}).
+				Foreground(lipgloss.AdaptiveColor{Light: "#61c77d", Dark: "#61c77d"}).
+				Padding(0, 0, 0, 1)
+	ChoosenDescStyle = ChoosenTitleStyle.Copy().
+				Foreground(lipgloss.AdaptiveColor{Light: "#84f5a2", Dark: "#84f5a2"})
 )
