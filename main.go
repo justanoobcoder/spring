bd95bb5e293d5479df9494ec2483d75795ea9f2f
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-16 16:05:43 +0700

    removed debug code block

--- main.go
+++ main.go
@@ -9,15 +9,6 @@ import (
 )
 
 func main() {
-	if len(os.Getenv("DEBUG")) > 0 {
-		f, err := tea.LogToFile("debug.log", "debug")
-		if err != nil {
-			fmt.Println("Error creating debug log file:", err)
-			os.Exit(1)
-		}
-		defer f.Close()
-	}
-
 	p := tea.NewProgram(ui.NewModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Error running program:", err)
