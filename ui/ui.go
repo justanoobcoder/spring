cf587ad96d98c15ea3acd49ab42dfd5ec015d962
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-16 08:39:21 +0700

    move style out of ui

--- ui/ui.go
+++ ui/ui.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/justanoobcoder/spring/ui/style"
+	"github.com/justanoobcoder/spring/style"
 )
 
 func (m Model) Init() tea.Cmd {
